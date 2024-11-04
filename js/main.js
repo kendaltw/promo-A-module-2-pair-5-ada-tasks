df2f6f422bc2ddd9060915b8149bf85302bad7e9
Tania <nadsanp@gmail.com>
2024-11-04 11:24:26 +0100

    add tasks to list through button search

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 const GITHUB_USER = "kendaltw";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 const list = document.querySelector(".js-list");
+const taskName = document.querySelector(".js-task");
+const button = document.querySelector(".js-button");
 let tasks = [];
 
 fetch(SERVER_URL)
@@ -22,6 +24,23 @@ function renderTasks() {
         list.innerHTML += `<li><input id=${task.id} type="checkbox"><p>${task.name}</p></li>`
     }
 }
+/* Cuando la usuaria haga click:
+    - recoger la tarea que ha escrito
+    - pintarla en el listado
+
+*/
+const addClick = (event) => {
+    event.preventDefault();
+    const nameTask = {
+        name: taskName.value,
+        completed: false,
+    };
+    list.innerHTML += `<li><input type="checkbox"><p>${nameTask.name.completed}</p></li>`
+    
+    
+
+} 
+
 
 
 /*Cuando la usuaria haga click en la casilla de la tarea
@@ -31,10 +50,11 @@ function renderTasks() {
 const handleClickList = (event) => {
     const taskId = parseInt(event.target.id);
     if (!taskId) return;
-    const underline =
+    //const underline =
     //list.classList.add("list");
 
-};
+}
+
 /*const checkList = handleClickList.map((task) =>{
     console.log(checkList);
         
@@ -44,6 +64,12 @@ const handleClickList = (event) => {
 */
 
 list.addEventListener("click", handleClickList);
+
+
+
+button.addEventListener("click", addClick);
+
+
 
 
 
